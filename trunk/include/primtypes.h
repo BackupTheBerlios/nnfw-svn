abf222a21046a1c686d1aec7ea112f86646cc894
mave <mave@93d2d01d-cdff-0310-9438-b6324f8bee6e>
2006-08-02 15:53:29 +0000

    Reorganized types.h
    A    include/primtypes.h

---
+++ trunk/include/primtypes.h
@@ -0,0 +1,44 @@
+/********************************************************************************
+ *  Neural Network Framework.                                                   *
+ *  Copyright (C) 2005-2006 Gianluca Massera <emmegian@yahoo.it>                     *
+ *                                                                              *
+ *  This program is free software; you can redistribute it and/or modify        *
+ *  it under the terms of the GNU General Public License as published by        *
+ *  the Free Software Foundation; either version 2 of the License, or           *
+ *  (at your option) any later version.                                         *
+ *                                                                              *
+ *  This program is distributed in the hope that it will be useful,             *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               *
+ *  GNU General Public License for more details.                                *
+ *                                                                              *
+ *  You should have received a copy of the GNU General Public License           *
+ *  along with this program; if not, write to the Free Software                 *
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA  *
+ ********************************************************************************/
+
+#ifndef PRIMTYPES_H
+#define PRIMTYPES_H
+
+/*! \file
+ *  \brief This file contains definition of primitive types ; Don't include this file directly, instead include types.h
+ *  Details...
+ */
+
+//! Namespace that contains all classes of Neural Network Framework
+namespace nnfw {
+
+//! Unsigned integer
+typedef  unsigned int u_int;
+
+//! Abstraction on the type of real numbers
+#ifndef NNFW_DOUBLE_PRECISION
+typedef float Real;
+#else
+typedef double Real;
+#endif
+
+}
+
+#endif
+
