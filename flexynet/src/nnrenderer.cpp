247315a76479199af540ecb0444a79789452fb3d
mave <mave@93d2d01d-cdff-0310-9438-b6324f8bee6e>
2007-05-07 13:09:50 +0000

    M    trunk/include/biasedcluster.h M    trunk/include/vectordata.h M    trunk/include/blockslearning.h M    trunk/include/matrixdata.h M    trunk/include/memutils.h M    trunk/include/nnfwfactory.h M    trunk/include/ionnfw.h M    trunk/include/utils.h M    trunk/include/updatable.h M    trunk/include/types.h M    trunk/include/outputfunction.h M    trunk/include/learningalgorithm.h M    trunk/include/propertized.h M    trunk/include/nnfw.h M    trunk/include/learningnetwork.h M    trunk/include/matrixlinker.h M    trunk/include/copylinker.h M    trunk/include/liboutputfunctions.h M    trunk/include/cluster.h M    trunk/include/derivableoutputfunction.h M    trunk/include/simplecluster.h M    trunk/include/dotlinker.h M    trunk/include/primtypes.h M    trunk/include/libmatrixmodifiers.h M    trunk/include/neuralnet.h M    trunk/include/ddecluster.h M    trunk/include/modifier.h M    trunk/include/observ.h M    trunk/include/messages.h M    trunk/include/fakecluster.h M    trunk/include/libradialfunctions.h M    trunk/include/clonable.h M    trunk/include/random.h M    trunk/include/normlinker.h M    trunk/include/realvec.h M    trunk/include/linker.h M    trunk/include/realmat.h M    trunk/include/sparsematrixlinker.h M    trunk/src/ddecluster.cpp M    trunk/src/utils.cpp M    trunk/src/nnfwfactory.cpp M    trunk/src/ionnfw.cpp M    trunk/src/updatable.cpp M    trunk/src/types.cpp M    trunk/src/outputfunction.cpp M    trunk/src/propertized.cpp M    trunk/src/messages.cpp M    trunk/src/fakecluster.cpp M    trunk/src/learningnetwork.cpp M    trunk/src/matrixlinker.cpp M    trunk/src/libradialfunctions.cpp M    trunk/src/copylinker.cpp M    trunk/src/liboutputfunctions.cpp M    trunk/src/normlinker.cpp M    trunk/src/random.cpp M    trunk/src/cluster.cpp M    trunk/src/realvec.cpp M    trunk/src/linker.cpp M    trunk/src/realmat.cpp M    trunk/src/sparsematrixlinker.cpp M    trunk/src/biasedcluster.cpp M    trunk/src/xmlnnfw.cpp M    trunk/src/dotlinker.cpp M    trunk/src/simplecluster.cpp M    trunk/src/neuralnet.cpp M    trunk/src/libmatrixmodifiers.cpp M    trunk/src/blockslearning.cpp M    flexynet/include/linkerrenderer.h M    flexynet/include/mainWindow.h M    flexynet/include/nnrenderer.h M    flexynet/include/clusterrenderer.h M    flexynet/src/nnrenderer.cpp M    flexynet/src/main.cpp M    flexynet/src/clusterrenderer.cpp M    flexynet/src/linkerrenderer.cpp M    flexynet/src/mainWindow.cpp

--- flexynet/src/nnrenderer.cpp
+++ flexynet/src/nnrenderer.cpp
@@ -1,3 +1,21 @@
+/********************************************************************************
+ *  Neural Network Framework.                                                   *
+ *  Copyright (C) 2005-2007 Gianluca Massera <emmegian@yahoo.it>                *
+ *                                                                              *
+ *  This program is free software; you can redistribute it and/or modify        *
+ *  it under the terms of the GNU General Public License as published by        *
+ *  the Free Software Foundation; either version 2 of the License, or           *
+ *  (at your option) any later version.                                         *
+ *                                                                              *
+ *  This program is distributed in the hope that it will be useful,             *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               *
+ *  GNU General Public License for more details.                                *
+ *                                                                              *
+ *  You should have received a copy of the GNU General Public License           *
+ *  along with this program; if not, write to the Free Software                 *
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA  *
+ ********************************************************************************/
 
 #include "nnrenderer.h"
 #include "linkerrenderer.h"
@@ -137,9 +155,9 @@ void NNRenderer::wheelEvent(QWheelEvent *event) {
 void NNRenderer::drawBackground(QPainter *painter, const QRectF &rect) {
 	QGraphicsView::drawBackground( painter, rect );
 
-	painter->setBrush(Qt::black);
-	painter->drawLine( -30, 0, +30, 0 );
-	painter->drawLine( 0, -30, 0, +30 );
+// 	painter->setBrush(Qt::black);
+// 	painter->drawLine( -30, 0, +30, 0 );
+// 	painter->drawLine( 0, -30, 0, +30 );
 
 	// draw SceneRect
 /*	painter->setBrush(Qt::transparent);
--- trunk/include/primtypes.h
+++ trunk/include/primtypes.h
@@ -1,6 +1,6 @@
 /********************************************************************************
  *  Neural Network Framework.                                                   *
- *  Copyright (C) 2005-2006 Gianluca Massera <emmegian@yahoo.it>                     *
+ *  Copyright (C) 2005-2007 Gianluca Massera <emmegian@yahoo.it>                     *
  *                                                                              *
  *  This program is free software; you can redistribute it and/or modify        *
  *  it under the terms of the GNU General Public License as published by        *
